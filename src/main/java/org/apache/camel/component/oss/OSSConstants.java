d01af3862ab84da2432d4091abc98dab58e4e7c6
duxg <2622519606@qq.com>
2020-07-24 10:19:28 +0800

    cs

---
+++ src/main/java/org/apache/camel/component/oss/OSSConstants.java
@@ -0,0 +1,26 @@
+package org.apache.camel.component.oss;
+
+public interface OSSConstants {
+    String BUCKET_NAME = "CamelAwsS3BucketName";
+    String BUCKET_DESTINATION_NAME = "CamelAwsS3BucketDestinationName";
+    String CACHE_CONTROL = "CamelAwsS3ContentControl";
+    String CONTENT_DISPOSITION = "CamelAwsS3ContentDisposition";
+    String CONTENT_ENCODING = "CamelAwsS3ContentEncoding";
+    String CONTENT_LENGTH = "CamelAwsS3ContentLength";
+    String CONTENT_MD5 = "CamelAwsS3ContentMD5";
+    String CONTENT_TYPE = "CamelAwsS3ContentType";
+    String E_TAG = "CamelAwsS3ETag";
+    String KEY = "CamelAwsS3Key";
+    String DESTINATION_KEY = "CamelAwsS3DestinationKey";
+    String LAST_MODIFIED = "CamelAwsS3LastModified";
+    String STORAGE_CLASS = "CamelAwsS3StorageClass";
+    String VERSION_ID = "CamelAwsS3VersionId";
+    String CANNED_ACL = "CamelAwsS3CannedAcl";
+    String ACL = "CamelAwsS3Acl";
+    String S3_OPERATION = "CamelAwsS3Operation";
+    String SERVER_SIDE_ENCRYPTION = "CamelAwsS3ServerSideEncryption";
+    String EXPIRATION_TIME = "CamelAwsS3ExpirationTime";
+    String REPLICATION_STATUS = "CamelAwsS3ReplicationStatus";
+    String RANGE_START = "CamelAwsS3RangeStart";
+    String RANGE_END = "CamelAwsS3RangeEnd";
+}
